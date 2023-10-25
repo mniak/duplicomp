064a57da608814307c0dbc2f88a4bfee538e9cea
Andre Soares <andregsilv@gmail.com>
2023-10-24 22:47:55 -0300

    comparator

--- comparator.go
+++ comparator.go
@@ -3,7 +3,7 @@ package duplicomp
 //go:generate mockgen -package=duplicomp -destination=mock_comparator_test.go . Comparator
 type Comparator interface {
 	Compare(
-		msg1 []byte, err1 error,
-		msg2 []byte, err2 error,
+		data1 []byte, err1 error,
+		data2 []byte, err2 error,
 	) error
 }
